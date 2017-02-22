def3ba9bafc6889303861f6244fe33189f9a9dc1
katebron <kate.bronstad@tufts.edu>
2017-02-21 20:27:49 -0500

    reduce content header, fix active bg color link

--- wp-content/themes/flat-bootstrap-child/js/theme.js
+++ wp-content/themes/flat-bootstrap-child/js/theme.js
@@ -10,7 +10,7 @@
 
 	// Touchscreen swipe through carousel (slider)
 	$(document).ready(function() {  
-	  //console.log("child working.");
+
       //$('#post-587 h2').prepend('<span class="glyphicon glyphicon-user"></span>');
      //$('#post-587 h2').prepend('<svg class="icon icon-organization"><use xlink:href="icons/symbol-defs.svg#icon-organization"></use></svg><span class="name"> icon-organization</span>');
 	// Put anything added to content by plugins at the end. This way our sub-pages and
