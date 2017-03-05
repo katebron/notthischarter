29d510257ddf254dbeab824a31df36fa90fcc52f
katebron <kate.bronstad@tufts.edu>
2017-03-05 08:08:21 -0500

    apply icon to new front post

--- wp-content/themes/flat-bootstrap-child/js/theme.js
+++ wp-content/themes/flat-bootstrap-child/js/theme.js
@@ -15,7 +15,7 @@
       $('#post-587 h2').prepend('<svg class="icon icon-person"><use xlink:href="#icon-person"></use></svg>');
       $('#post-589 h2').prepend('<svg class="icon icon-organization"><use xlink:href="#icon-organization"></use></svg>');
       $('#post-591 h2').prepend('<svg class="icon icon-vote"><use xlink:href="#icon-vote"></use></svg>');
-      $('#post-593 h2').prepend('<svg class="icon icon-presentation"><use xlink:href="#icon-presentation"></use></svg>');
+      $('#post-974 h2').prepend('<svg class="icon icon-presentation"><use xlink:href="#icon-presentation"></use></svg>');
 
 	// Put anything added to content by plugins at the end. This way our sub-pages and
 	// recent post sections, etc. appear before it.
